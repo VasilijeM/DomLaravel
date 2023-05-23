2ff1e1cb353cd4b580f1d01c455c5cf87e24cf6c
VasilijeM <vasilijemladenovic.vasa@gmail.com>
2023-05-23 11:54:08 +0200

    Ad, type, promotion model, napunjena baza

---
+++ app/Models/Ad.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ad extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'naslov',
+        'cena',
+        'opis',
+        'pregledi',
+        'type_id',
+        'user_id'
+    ];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function promotions()
+    {
+        return $this->hasMany(Promotion::class);
+    }
+}
